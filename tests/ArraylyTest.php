fe8367573fdc988fc70b9649fea3e5acf658de9f
bastman <gtgames88>
2017-05-08 12:57:14 +0200

    - tests

--- tests/ArraylyTest.php
+++ tests/ArraylyTest.php
@@ -4,6 +4,16 @@ use Arrayly\Arrayly as A;
 class ArraylyTestCase extends PHPUnit_Framework_TestCase
 {
 
+    private function provideTestCities():array {
+        return $cities = [
+            ["city" => "Berlin", "country" => "Germany"],
+            ["city" => "Hamburg", "country" => "Germany"],
+            ["city" => "London", "country" => "England"],
+            ["city" => "Manchester", "country" => "England"],
+            ["city" => "Paris", "country" => "France"],
+        ];
+    }
+
     public function testConstruct()
     {
         $arrayly = new A(["foo" => "bar"]);
@@ -14,5 +24,17 @@ class ArraylyTestCase extends PHPUnit_Framework_TestCase
     public function assertArrayly($actual)
     {
         $this->assertInstanceOf('Arrayly\Arrayly', $actual);
+    }
+
+    public function testMap() {
+        $source = $this->provideTestCities();
+
+        $sink=A::ofArray($source)
+            ->map(function($v) {
+                return $v["city"];
+            })->toArray();
+
+
+        $this->assertSame(["Berlin", "Hamburg", "London", "Manchester", "Paris"], $sink);
     }
 }
