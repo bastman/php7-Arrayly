6f19044975fb6f0b3148309272802a05f280f683
bastman <gtgames88>
2017-05-17 12:53:57 +0200

    - restructure

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/arrayly.fn.php';
+require_once __DIR__ . '/fn/arrayly.fn.php';
 require_once __DIR__ . '/Sequence/sequence.gen.php';
 require_once __DIR__ . '/Pipeline/pipeline.fn.php';
 require_once __DIR__ . '/Create.php';
