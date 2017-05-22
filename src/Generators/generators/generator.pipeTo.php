ff17154e638a1d491b9eede24144743c7a6f2413
bastman <gtgames88>
2017-05-22 18:19:39 +0200

    make api's consistent

--- src/Generators/generators/generator.pipeTo.php
+++ src/Generators/generators/generator.pipeTo.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 namespace Arrayly\Generators\generators;
 use Arrayly\Util\internals as utils;
 
-// Is there any use case for that?
-function iterate(iterable $iterable): \Generator
-{
-    foreach ($iterable as $k => $v) {
-        yield $k => $v;
-    }
-}
 
 function pipeTo(iterable $iterable, \Closure $transform): \Generator
 {
--- src/Generators/partials/partial.pipeTo.php
+++ src/Generators/partials/partial.pipeTo.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 namespace Arrayly\Generators\partials;
 use Arrayly\Generators\generators as generate;
 
-// Is there any use case for that?
-function iterate(): \Closure
-{
-    return function (iterable $iterable) {
-        return generate\iterate($iterable);
-    };
-}
-
 function pipeTo(\Closure $transform): \Closure
 {
     return function (iterable $iterable) use ($transform){
